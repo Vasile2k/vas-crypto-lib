1d9c57a61fb00436227b59b42970b7faa8e35233
Vasile2kappa <andy.dorcu@yahoo.com>
2021-05-11 16:32:03 +0300

    Test for ECB and CBC

---
+++ test/test_encrypter.ts
@@ -0,0 +1,51 @@
+import { strict as assert } from "assert";
+
+import { DataEncrypter, ModeOfOperation } from "../data_encrypter";
+import { RC6EncryptionAlgorithm } from "../encryption";
+
+describe("encrypter", () => {
+
+    it("should encrypt and decrypt a big blob in ECB", () => {
+        let textEncoder = new TextEncoder();
+
+        let blob = textEncoder.encode("afara ploo cal uaie cazan pedale gigel vasilica Cadavru frumos urat se poate nu se poate da");
+        let key = textEncoder.encode("uaieccaluaieccal");
+
+        let enc = new DataEncrypter();
+        enc.setModeOfOperation(ModeOfOperation.ECB);
+        enc.setEncryptionAlgorithm(new RC6EncryptionAlgorithm());
+
+        let encrypted = enc.encryptBlob(blob, key);
+
+        let decrypted = enc.decryptBlob(encrypted, key);
+
+        // length is not important since the blob will be padded
+
+        blob.every((val, i) => {
+            assert.equal(val, decrypted[i], "Decrypted doesn't match plaintext.");
+        });
+    });
+
+    it("should encrypt and decrypt a big blob in CBC", () => {
+        let textEncoder = new TextEncoder();
+
+        let blob = textEncoder.encode("afara ploo cal uaie cazan pedale gigel vasilica Cadavru frumos urat se poate nu se poate da");
+        let key = textEncoder.encode("uaieccaluaieccal");
+
+        let enc = new DataEncrypter();
+        enc.setModeOfOperation(ModeOfOperation.CBC);
+        enc.setEncryptionAlgorithm(new RC6EncryptionAlgorithm());
+        enc.setInitializationVector(new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]));
+
+        let encrypted = enc.encryptBlob(blob, key);
+
+        let decrypted = enc.decryptBlob(encrypted, key);
+
+        // length is not important since the blob will be padded
+
+        blob.every((val, i) => {
+            assert.equal(val, decrypted[i], "Decrypted doesn't match plaintext.");
+        });
+    });
+
+});
