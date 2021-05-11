03b558ecc6425980654bfbdc5018b456f782982f
Vasile2kappa <andy.dorcu@yahoo.com>
2021-05-11 16:37:39 +0300

    Update exports

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
-import { EncryptionAlgorithm, RC6EncryptionAlgorithm } from "./encryption";
+import { EncryptionAlgorithm, RC6EncryptionAlgorithm, RC4EncryptionAlgorithm } from "./encryption";
 import { ModeOfOperation, DataEncrypter } from "./data_encrypter";
 
 export {
     EncryptionAlgorithm,
     RC6EncryptionAlgorithm,
+    RC4EncryptionAlgorithm,
     ModeOfOperation,
     DataEncrypter
 };
