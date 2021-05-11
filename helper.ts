2f22ca097d8c1805045830ac3f281625659775e6
Vasile2kappa <andy.dorcu@yahoo.com>
2021-05-11 16:31:32 +0300

    Add bytewise xor

--- helper.ts
+++ helper.ts
@@ -9,6 +9,15 @@ function rotateRightInt32(num: number, pos: number): number{
     return ((num >>> pos) | (num << (32 - pos))) & (0xFFFFFFFF);
 }
 
+// For internal use only so no need to check sizes
+function xor(block1: Uint8Array, block2: Uint8Array): Uint8Array{
+    let blockFinal = new Uint8Array(block1);
+    for(let i = 0; i < blockFinal.length; ++i){
+        blockFinal[i] ^= block2[i];
+    }
+    return blockFinal;
+}
+
 export {
-    rotateLeftInt32, rotateRightInt32
+    rotateLeftInt32, rotateRightInt32, xor
 };
